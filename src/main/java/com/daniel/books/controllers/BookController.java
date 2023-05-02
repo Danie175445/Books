fedf0a062ee5b3d38c3a124130f3b01960a5d45c
Daniel Castillo <danny4004@gmail.com>
2023-05-02 00:21:10 -0700

    rendering books

---
+++ src/main/java/com/daniel/books/controllers/BookController.java
@@ -0,0 +1,28 @@
+package com.daniel.books.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.daniel.books.models.Book;
+import com.daniel.books.services.BookService;
+
+@Controller
+public class BookController {
+	
+		@Autowired
+		BookService bookService;
+
+		@RequestMapping("/book/{bookId}")
+		public String index(Model model,@PathVariable ("bookId") Long bookId) {
+			System.out.println(bookId);
+			Book book = bookService.findBook(bookId);
+			System.out.println(book);
+			model.addAttribute("book", book);
+			
+			return "index.jsp";
+		}
+
+}
